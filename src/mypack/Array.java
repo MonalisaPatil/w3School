c9df72fb862549a697669b8e0d89fb3a23961178
Monalisa Patil <90000984+MonalisaPatil@users.noreply.github.com>
2024-07-12 20:30:44 -0500

    first commit

--- src/mypack/Array.java
+++ src/mypack/Array.java
@@ -26,7 +26,7 @@ public class Array {
         }
     }
 
-
+// Write a program to build a random array by accepting an elements from a user and then convert int array to char array.
 
 
 
--- src/mypack/ArrayQ1.java
+++ src/mypack/ArrayQ1.java
@@ -1,4 +1,34 @@
 package mypack;
 
+//Write a program to accept a strings from user,
+// And  split it by spaces
+// traverse through individual string and find the count of a character that user has asked to search
+
+import java.util.Scanner;
+
 public class ArrayQ1 {
+    public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+        System.out.println("Enter sentence:");
+    String input = s.nextLine();
+        System.out.println(input);
+
+    String [] space =  input.split(" ");
+        System.out.println("enter alphabet to count");
+        String  alphabet = s.next();
+        char word = alphabet.charAt(0);
+        int count = 0;
+       for (String i:space ){
+           System.out.println(i);
+           for (int a = 0 ; a<= i.length()-1; a++){
+               if (i.charAt(a)==word){
+                   count++;
+               }
+           }
+       }
+        System.out.println(count);
+
+}
+
+
 }
